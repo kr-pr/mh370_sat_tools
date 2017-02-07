2a72147ef9e27d31c6439fe9aec185a588ced908
Kirill Prostyakov <kprostyakov@gmail.com>
2017-02-08 01:31:06 +0300

    teme to ecef works

---
+++ src/tle/astro.py
@@ -0,0 +1,40 @@
+'Helper functions for time and coordinate conversion from Vallado matlab codes'
+from math import floor, fmod, pi, cos, sin
+from numpy import array, dot, cross
+
+
+def jd(*time_ut1):
+    year, month, day, hour, minute, second = time_ut1
+    jday = 367.0 * year - floor((7 * (year + floor((month + 9) / 12.0)))* 0.25) + \
+        floor(275 * month / 9.0) + day + 1721013.5 + (second + minute * 60.0 + hour * 3600.0) / 86400.0
+    return jday
+
+
+def gmst(lod, jul_day):
+    tut1 = (jul_day - 2451545.0) / 36525.0
+    seconds = -6.2e-6 * tut1 * tut1 * tut1 + 0.093104 * tut1 * tut1 + \
+        (876600.0 * 3600.0 + 8640184.812866) * tut1 + 67310.54841
+    print(tut1,seconds)
+    _theta = 2 * pi * fmod(seconds / 86400.0, 1.0)
+    theta = _theta if _theta>0 else _theta + 2*pi
+    omega = 7.29211514670698e-5 * (1.0 - lod / 86400.0)
+
+    return theta, omega
+
+
+def teme_to_ecef(r_teme, v_teme, x_p, y_p, theta, omega):
+    s_x = sin(x_p)
+    c_x = cos(x_p)
+    s_y = sin(y_p)
+    c_y = cos(y_p)
+    s_z = sin(theta)
+    c_z = cos(theta)
+    rot_x = array(([1.0, 0, 0], [0, c_y, -s_y], [0, s_y, c_y]))
+    rot_y = array(([c_x, 0, s_x], [0, 1.0, 0], [-s_x, 0, c_x]))
+    rot_z = array(([c_z, -s_z, 0], [s_z, c_z, 0], [0, 0, 1.0]))
+
+    xy = dot(rot_x, rot_y)
+    r_ecef = r_teme.dot(rot_z).dot(xy)
+    v_ecef = (v_teme - cross(array([0, 0, omega]), r_teme)).dot(rot_z).dot(xy)
+
+    return r_ecef, v_ecef
--- src/tle/make_tle.py
+++ src/tle/make_tle.py
@@ -1,29 +1,83 @@
-'Fits TLE to known satellite position'
-from scipy import optimize
+'Fits TLE orbit elements to known satellite positions in ECEF'
+from re import findall
+from math import pi
+#from numpy import array
+#from scipy import optimize
 
-def parse_tle(tle):
-    return {}
+def elem_from_tle(tle):
+    'Extracts orbital elements from TLE'
+    lines = tle.split('\n')
+    elements = (lines[2][8:16], lines[2][17:25], lines[2][26:33],
+                lines[2][34:42], lines[2][43:51], lines[2][52:63])
+    return [float(x) for x in elements]
 
-def position_from_csv(file_name):
-    return []
+def elem_to_tle(tle, *elems):
+    'Saves elements into TLE string'
+    if len(elems) == 6:
+        lines = tle.split('\n')
+        elem_list = list(elems)
+        elem_list[2] = int(elems[2])
+        newline = lines[2][:8]+'{:08.4f} {:08.4f} {:07d} {:08.4f} {:08.4f} {:011.8f}'.format(
+            *elem_list)+lines[2][63:-1]
+        checksum = str(sum([int(x) for x in findall(r'\d', newline)]) % 10)
+        lines[2] = newline+checksum
+    else:
+        raise ValueError
+    new_tle = '\n'.join(lines)
+    return new_tle
 
-def make_residual_func(data):
-    def residual_func():
-        nonlocal data
-        res = None
+def earth_constants_from_text(file_name):
+    'Loads earth movement constants from IERF file'
+    with open('data/'+file_name, 'rt') as fin:
+        line = next(fin)
+        while not line[:6] == '14 3 7':
+            line = next(fin)
+        x_pm = float(line[18:27])*pi/648000.0
+        y_pm = float(line[37:46])*pi/648000.0
+        dut = float(line[58:68])
+        lod = float(line[79:86])*1e-3
+    return (dut, x_pm, y_pm, dut, lod)
+
+def ecef_state_from_csv(file_name):
+    'Loads Inmarsat satellite state vectors'
+    with open('data/'+file_name, 'rt') as fin:
+        data = []
+        for line in fin:
+            items = line.split(' ')
+            time = [int(item) for item in items[0].split(':')]
+            day = 8 if time[0] == 0 else 7
+            date = [2014, 3, day]+time
+            state = [float(item) for item in items[1:]]
+            data.append((date, state))
+    return data
+
+def make_residual_func(ecef_state, orig_elems):
+    def residual_func(*factors):
+        nonlocal ecef_state, orig_elems
+        new_state = [factors[i]*orig_elems[i] for i in range(6)]
+        res = 0
+        for date, state in ecef_state:
+            teme_state = predict(tle)
+            res += 0 
         return res
     return residual_func
 
-def fit_tle():
 
-def save_tle():
+def predict(tle):
+
+    return None
+
+def TEME_to_IERF(teme_r, teme_v)
+    return None
 
 def main():
     'Runs calculation chain'
     tle = '''0 INMARSAT 3-F1
 1 23839U 96020A   14066.96754476 -.00000012  00000-0  10000-3 0  9995
 2 23839 001.6371 073.1994 0005326 270.3614 234.8362 01.00274124 65669'''
-    elements = parse_tle(tle)
+    orig_elems = elem_from_tle(tle)
+
+    sat_state_ecef = ecef_state_from_csv('ecef.txt')
 
 
 if __name__ == '__main__':
