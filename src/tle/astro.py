5af05af3b38ff848c7ebb0fcf2a9a1cc42bba99d
Kirill Prostyakov <kprostyakov@gmail.com>
2017-02-08 13:49:50 +0300

    optimized TLE

--- src/tle/astro.py
+++ src/tle/astro.py
@@ -1,28 +1,31 @@
 'Helper functions for time and coordinate conversion from Vallado matlab codes'
 from math import floor, fmod, pi, cos, sin
-from numpy import array, dot, cross
-
+from numpy import array, cross
 
 def jd(*time_ut1):
+    'Computes Julian Date'
     year, month, day, hour, minute, second = time_ut1
     jday = 367.0 * year - floor((7 * (year + floor((month + 9) / 12.0)))* 0.25) + \
-        floor(275 * month / 9.0) + day + 1721013.5 + (second + minute * 60.0 + hour * 3600.0) / 86400.0
+        floor(275 * month / 9.0) + day + 1721013.5 + \
+        (second + minute * 60.0 + hour * 3600.0) / 86400.0
     return jday
 
 
 def gmst(lod, jul_day):
+    '''Computes smoothed Greenwich siderial time:
+    http://articles.adsabs.harvard.edu//full/1982A%26A...105..359A/0000360.000.html'''
     tut1 = (jul_day - 2451545.0) / 36525.0
     seconds = -6.2e-6 * tut1 * tut1 * tut1 + 0.093104 * tut1 * tut1 + \
         (876600.0 * 3600.0 + 8640184.812866) * tut1 + 67310.54841
-    print(tut1,seconds)
     _theta = 2 * pi * fmod(seconds / 86400.0, 1.0)
-    theta = _theta if _theta>0 else _theta + 2*pi
+    theta = _theta if _theta > 0 else _theta + 2*pi
     omega = 7.29211514670698e-5 * (1.0 - lod / 86400.0)
 
     return theta, omega
 
 
 def teme_to_ecef(r_teme, v_teme, x_p, y_p, theta, omega):
+    'Rotates coordinates to ECEF'
     s_x = sin(x_p)
     c_x = cos(x_p)
     s_y = sin(y_p)
@@ -33,8 +36,8 @@ def teme_to_ecef(r_teme, v_teme, x_p, y_p, theta, omega):
     rot_y = array(([c_x, 0, s_x], [0, 1.0, 0], [-s_x, 0, c_x]))
     rot_z = array(([c_z, -s_z, 0], [s_z, c_z, 0], [0, 0, 1.0]))
 
-    xy = dot(rot_x, rot_y)
-    r_ecef = r_teme.dot(rot_z).dot(xy)
-    v_ecef = (v_teme - cross(array([0, 0, omega]), r_teme)).dot(rot_z).dot(xy)
+    rot_xyz = rot_z.dot(rot_x.T).dot(rot_y.T)
+    r_ecef = r_teme.dot(rot_xyz)
+    v_ecef = (v_teme - cross(array([0, 0, omega]), r_teme)).dot(rot_xyz)
 
     return r_ecef, v_ecef
--- src/tle/make_tle.py
+++ src/tle/make_tle.py
@@ -1,15 +1,19 @@
 'Fits TLE orbit elements to known satellite positions in ECEF'
 from re import findall
 from math import pi
-#from numpy import array
-#from scipy import optimize
+from numpy import array, multiply, zeros
+from scipy.optimize import minimize
+from sgp4.earth_gravity import wgs72
+from sgp4.io import twoline2rv
+
+import astro
 
 def elem_from_tle(tle):
     'Extracts orbital elements from TLE'
     lines = tle.split('\n')
     elements = (lines[2][8:16], lines[2][17:25], lines[2][26:33],
                 lines[2][34:42], lines[2][43:51], lines[2][52:63])
-    return [float(x) for x in elements]
+    return array([float(x) for x in elements])
 
 def elem_to_tle(tle, *elems):
     'Saves elements into TLE string'
@@ -36,7 +40,7 @@ def earth_constants_from_text(file_name):
         y_pm = float(line[37:46])*pi/648000.0
         dut = float(line[58:68])
         lod = float(line[79:86])*1e-3
-    return (dut, x_pm, y_pm, dut, lod)
+    return x_pm, y_pm, dut, lod
 
 def ecef_state_from_csv(file_name):
     'Loads Inmarsat satellite state vectors'
@@ -47,38 +51,70 @@ def ecef_state_from_csv(file_name):
             time = [int(item) for item in items[0].split(':')]
             day = 8 if time[0] == 0 else 7
             date = [2014, 3, day]+time
-            state = [float(item) for item in items[1:]]
+            state = array([float(item) for item in items[1:]])
             data.append((date, state))
     return data
 
-def make_residual_func(ecef_state, orig_elems):
-    def residual_func(*factors):
-        nonlocal ecef_state, orig_elems
-        new_state = [factors[i]*orig_elems[i] for i in range(6)]
+def make_teme_ecef_conv_func(*constants):
+    'Closure for ECEF conversion function'
+    def teme_ecef_conv(r_t, v_t, *time_utc):
+        'Converts from TEME to ECEF'
+        nonlocal constants
+        x_pm, y_pm, dut, lod = constants
+        time_ut1 = list(time_utc)
+        time_ut1[-1] += dut
+        jul_day = astro.jd(*time_ut1)
+        theta, omega = astro.gmst(lod, jul_day)
+        r_e, v_e = astro.teme_to_ecef(r_t, v_t, x_pm, y_pm, theta, omega)
+        return r_e, v_e
+    return teme_ecef_conv
+
+def tle_from_factors(tle, factors, orig_elems):
+    weights = array([1.0,1.0,10000.0,1.0,1.0,1e-4])
+    new_state = orig_elems+multiply(factors, weights)
+    new_tle = elem_to_tle(tle, *list(new_state))
+    return new_tle
+
+def make_residual_func(tle, ecef_state, orig_elems, conv_func):
+    'Closure for least squares residual function'
+    def residual_func(factors):
+        'Calculates sum of squared residuals over all known state vectors'
+        nonlocal tle, ecef_state, orig_elems, conv_func
+        new_tle = tle_from_factors(tle, factors, orig_elems)
         res = 0
-        for date, state in ecef_state:
-            teme_state = predict(tle)
-            res += 0 
+        for time, state in ecef_state:
+            r_t, v_t = predict(new_tle, *time)
+            r_e, v_e = conv_func(array(r_t), array(v_t), *time)
+            res_r = r_e - state[:3]
+            res_v = (v_e - state[3:])*1e3
+            res += res_r.dot(res_r) + res_v.dot(res_v)
+        print(factors)
         return res
     return residual_func
 
-
-def predict(tle):
-
-    return None
-
-def TEME_to_IERF(teme_r, teme_v)
-    return None
+def predict(tle, *time):
+    'Calculates state vector using SGP4 routines'
+    lines = tle.split('\n')
+    satellite = twoline2rv(lines[1], lines[2], wgs72)
+    position, velocity = satellite.propagate(*time)
+    return position, velocity
 
 def main():
     'Runs calculation chain'
+    constants = earth_constants_from_text('finals2000A.data')
+    ecef_conv_func = make_teme_ecef_conv_func(*constants)
+
     tle = '''0 INMARSAT 3-F1
 1 23839U 96020A   14066.96754476 -.00000012  00000-0  10000-3 0  9995
 2 23839 001.6371 073.1994 0005326 270.3614 234.8362 01.00274124 65669'''
-    orig_elems = elem_from_tle(tle)
-
+    orig_elements = elem_from_tle(tle)
     sat_state_ecef = ecef_state_from_csv('ecef.txt')
-
+    resid_func = make_residual_func(tle, sat_state_ecef, orig_elements, ecef_conv_func)
+    opt_factors = minimize(resid_func, zeros(6), method='BFGS', options={'maxiter':11, 'eps':1e-4})
+    opt_tle = tle_from_factors(tle, opt_factors.x, orig_elements)
+    with open('data/opt.tle', 'wt') as fout:
+        for line in opt_tle:
+            fout.write(line)
 
 if __name__ == '__main__':
     main()
