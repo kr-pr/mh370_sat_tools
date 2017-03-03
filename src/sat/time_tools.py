d901eecf6c7b85e8f648949911baf00a60fdb77a
Kirill Prostyakov <kprostyakov@gmail.com>
2017-03-03 17:51:45 +0300

    interpolation works

--- src/sat/time_tools.py
+++ src/sat/time_tools.py
@@ -2,9 +2,10 @@
 from datetime import timedelta
 from math import pi, exp
 from cmath import rect
-from numpy import array, angle, absolute, log, zeros_like
+from numpy import array, angle, absolute, log, zeros_like, arange
 from numpy.fft import rfft, irfft, rfftfreq
 from scipy.interpolate import splrep, splev
+from scipy.optimize import minimize
 from scipy import polyfit
 
 def make_plot(x_vals, y_vals):
@@ -37,26 +38,41 @@ def index_to_time(ind, time_ref, delta=5):
 def time_to_spectrum(t_series):
     'computes power spectral density of time series'
     spectrum = rfft(t_series)
+    #print(spectrum[:5])
     return log(absolute(spectrum)), angle(spectrum)
 
-def make_residual_func(t_max, slope, n_harm, samples, indices):
+def make_residual_func(samples, indices, **params):
     'closure for residual func'
-    freqs = rfftfreq(indices[-1], 5)
-    ind_from = int(round(1/(t_max*freqs[1])))
-    ind_to = ind_from+n_harm
+    fft_size = 2
+    while fft_size<indices[-1]:
+        fft_size*=2
+    freqs = rfftfreq(fft_size, 5)
+    ind_from = int(round(1/(params['t_max']*freqs[1])))
+    ind_to = ind_from+params['n_harm']
+    def make_series(x):
+        'Calculates time series from parameterized spectrum'
+        nonlocal freqs, ind_from, ind_to, params
+        spectrum = zeros_like(freqs, 'complex')
+        sign_x0 = 0 if x[0] == 0.5 else abs(x[0]-0.5)/(x[0]-0.5)
+        spectrum[0] = rect(sign_x0*exp(params['scale'][0]*abs(x[0]-0.5)), 0)
+        for i in range(ind_from, ind_to):
+            spectrum[i] = rect(
+                exp(params['scale'][1]*x[1]+params['slope']*log(freqs[i])),
+                params['scale'][2]*x[2+i-ind_from]
+            )
+        #print(x)
+        #print(spectrum[0], spectrum[ind_from:ind_to])
+        return irfft(spectrum)
     def residual_func(x):
         'calculates sum of squared residuals'
-        nonlocal freqs, ind_from, ind_to
-        spectrum = zeros_like(freqs, 'complex')
-        spectrum[0] = rect(10000.0 * x[0], 0)
-        for i in range(ind_from, ind_to):
-            spectrum[i] = rect(exp(x[1]+slope*log(freqs[i])), 2*pi*x[2+i])
-        series = irfft(spectrum)
+        nonlocal samples, indices
+        series = make_series(x)
         sum_err = 0
         for position, ind in enumerate(indices):
             sum_err += (series[ind]-samples[position])**2
+        print(series[:5])
         return sum_err
-    return residual_func
+    return make_series, residual_func
 
 def interp_helper(all_data, trend_data, time_from):
     'performs lf spline + hf fft interpolation of radial distance'
@@ -75,19 +91,31 @@ def interp_helper(all_data, trend_data, time_from):
     #make_plot_2(all_indices, trend, all_indices, array(all_values))
     #make_plot(all_indices, detrended)
     dense_samples = detrended[:split_time]
+    #print(min(detrended), max(detrended))
     sparse_samples = detrended[split_time:]
     sparse_indices = (all_indices[split_time:]-split_time).astype(int)
     #print(list(zip(sparse_indices,sparse_samples)))
     amp, _ = time_to_spectrum(dense_samples)
-    dense_freq = rfftfreq(dense_samples.size, 5)
+    #print(amp[:5])
+    dense_freq = rfftfreq(dense_samples.size, 5) 
     periods = (3000.0, 300.0)
     ind_from = int(round(1/(periods[0]*dense_freq[1])))
     ind_to = int(round(1/(periods[1]*dense_freq[1])))
-    loglog_a, _ = polyfit(log(dense_freq[ind_from:ind_to]), amp[ind_from:ind_to], 1)
+    slope, _ = polyfit(log(dense_freq[ind_from:ind_to]), amp[ind_from:ind_to], 1)
     #print(loglog_a, loglog_b)
     #make_plot_window(log(dense_freq[1:]), amp[1:], ind_from, ind_to)
-    n_harm = 5
-    residual_func = make_residual_func(periods[0], loglog_a, n_harm, sparse_samples, sparse_indices)
+    params = {
+        't_max': periods[0],
+        'slope': slope,
+        'n_harm': 21,
+        'scale': [20, 4, 2*pi]
+    }
+    series_func, residual_func = make_residual_func(sparse_samples, sparse_indices, **params)
+    x0 = array([0.5]*(params["n_harm"]+2))
+    bounds = [(0, 1)]*(params["n_harm"]+2)
+    result = minimize(residual_func, x0, method="L-BFGS-B", bounds=bounds, options={'eps':1e-3})
+    print(result.x)
+    make_plot_2(sparse_indices, sparse_samples, arange(sparse_indices[-1]), series_func(result.x)[:sparse_indices[-1]])
     interp_times = []
     interp_values = []
     return zip(interp_times, interp_values)
